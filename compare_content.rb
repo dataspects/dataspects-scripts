d11348c6f73cabf933023c0f7539c9e3297f21d7
Lex Sulzer <lex@sulzer.net>
2019-02-20 15:09:49 +0100

    WIP

--- compare_content.rb
+++ compare_content.rb
@@ -3,20 +3,18 @@ require_all '../dataspects/lib'
 require 'yaml'
 profiles = YAML.load_file('/home/lex/profiles.yml')
 
-label = 'team.findandlearn.net'
-
-ontology = "ConferenceManagementOntology"
-
+label = 'cookbook.findandlearn.net'
 mw = Dataspects::MediaWiki.new(
   url: profiles[label]['url'],
   user: profiles[label]['user'],
   password: profiles[label]['password'],
   log_in: :must_log_in
 )
-mw.resources_from_CATEGORY(ontology)
 
+ontology = "dataspectsSystemCoreOntology"
 re = Dataspects::OntologyRepository.new("../#{ontology}")
 re.use_existing_at_URL
 
+mw.resources_from_CATEGORY(ontology)
 c = Dataspects::Comparer.new
-c.report(mw, re)
+c.report(re, mw)
